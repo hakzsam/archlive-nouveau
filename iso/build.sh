ee35731231e218ce33032e4eb3b497ed24efdd76
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2014-10-21 23:46:38 +0000

    iso/build: Only build for x86_64

--- iso/build.sh
+++ iso/build.sh
@@ -231,14 +231,14 @@ mkdir -p ${work_dir}
 run_once make_pacman_conf
 
 # Do all stuff for each airootfs
-for arch in i686 x86_64; do
+for arch in x86_64; do
     run_once make_basefs
     run_once make_packages
     run_once make_setup_mkinitcpio
     run_once make_customize_airootfs
 done
 
-for arch in i686 x86_64; do
+for arch in x86_64; do
     run_once make_boot
 done
 
@@ -249,7 +249,7 @@ run_once make_isolinux
 run_once make_efi
 run_once make_efiboot
 
-for arch in i686 x86_64; do
+for arch in x86_64; do
     run_once make_prepare
 done
 
