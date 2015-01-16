cc0da2a61056555060e62c08761375a09da8e1fb
Pierre Moreau <pierre.morrow@free.fr>
2015-01-17 00:46:42 +0100

    build: Build as little as root as possible

--- build.sh
+++ build.sh
@@ -2,20 +2,33 @@
 
 archs=('x86_64')
 
+user="${1}"
+if [[ -z ${user} ]]; then
+    user=${USER}
+    if [[ ${EUID} -eq 0 ]]; then
+        echo "Warning: You should specify a username as first argument or run \
+the script as a normal user."
+        echo "Some parts requires root access (like building the image and \
+cleaning pacman's cache)."
+        echo "If you plan to build automatically, run the script as root and \
+give a normal username as first argument."
+    fi
+fi
+
 
 # Clean local repository
-cd repo && ./clean.sh
+cd repo && su ${USER} -c './clean.sh'
 cd ..
 
 # Clean image folder
-cd iso && ./clean.sh
+cd iso && su -c './clean.sh'
 cd ..
 
-pacman -Sc
+su -c 'pacman -Sc'
 
 
 # Build custom packages.
-cd pkg && ./build.sh ${archs}
+cd pkg && su ${USER} -c "./build.sh ${archs}"
 if [ $? -ne 0 ]; then
 	echo "Failed to build custom packages!"
 	exit 1
@@ -23,7 +36,7 @@ fi
 cd ..
 
 # Build the custom local repository.
-cd repo && ./build.sh ${archs}
+cd repo && su ${USER} -c "./build.sh ${archs}"
 if [ $? -ne 0 ]; then
 	echo "Failed to build the custom local repository!"
 	exit 1
@@ -31,7 +44,7 @@ fi
 cd ..
 
 # Build the latest Linux kernel and Nouveau DRM.
-cd src && ./build.sh
+cd src && su ${USER} -c "./build.sh"
 if [ $? -ne 0 ]; then
 	echo "Failed to build the latest Linux kernel or Nouveau DRM!"
 	exit 1
@@ -39,7 +52,7 @@ fi
 cd ..
 
 # Build the image.
-cd iso && ./build.sh -v ${archs}
+cd iso && su -c "./build.sh -v ${archs}"
 if [ $? -ne 0 ]; then
 	echo "Failed to build the image!"
 	exit 1
