1b1f6d26c9c2d2ac4c1d28a14de30f33c270a36a
Pierre Moreau <pierre.morrow@free.fr>
2015-01-16 23:20:23 +0100

    build: Remove dependency of exiting cd from preceding command
    If the command preceding the exiting cd fails, we still want to exit the
    directory.

--- build.sh
+++ build.sh
@@ -3,38 +3,47 @@
 archs=('x86_64')
 
 
-cd repo && ./clean.sh && cd ..
-cd iso && ./clean.sh && cd ..
+# Clean local repository
+cd repo && ./clean.sh
+cd ..
+
+# Clean image folder
+cd iso && ./clean.sh
+cd ..
 
 pacman -Sc
 
 
 # Build custom packages.
-cd pkg && ./build.sh ${archs} && cd -
+cd pkg && ./build.sh ${archs}
 if [ $? -ne 0 ]; then
 	echo "Failed to build custom packages!"
 	exit 1
 fi
+cd ..
 
 # Build the custom local repository.
-cd repo && ./build.sh ${archs} && cd -
+cd repo && ./build.sh ${archs}
 if [ $? -ne 0 ]; then
 	echo "Failed to build the custom local repository!"
 	exit 1
 fi
+cd ..
 
 # Build the latest Linux kernel and Nouveau DRM.
-cd src && ./build.sh && cd -
+cd src && ./build.sh
 if [ $? -ne 0 ]; then
 	echo "Failed to build the latest Linux kernel or Nouveau DRM!"
 	exit 1
 fi
+cd ..
 
 # Build the image.
-cd iso && ./build.sh -v ${archs} && cd -
+cd iso && ./build.sh -v ${archs}
 if [ $? -ne 0 ]; then
 	echo "Failed to build the image!"
 	exit 1
 fi
+cd ..
 
 exit 0
