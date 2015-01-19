b6ac4beb009616a929bbea3921da2a1013b54b26
Pierre Moreau <pierre.morrow@free.fr>
2015-01-19 12:50:37 +0100

    src: Do not build if no new commits

--- src/build.sh
+++ src/build.sh
@@ -12,30 +12,38 @@ nouveau_dir=nouveau
 if [ ! -d ${linux_dir} ]; then
     git clone ${linux_git} ${linux_dir} --depth 1 --branch ${linux_branch}
 else
-    cd ${linux_dir} && git pull origin ${linux_branch} && cd -
+    cd ${linux_dir}
+    res=`git pull origin ${linux_branch}`
+    cd -
 fi
 if [ $? -ne 0 ]; then
     echo "Failed to update 'linux' repository!"
     exit 1
 fi
 
-cd ${linux_dir}
-zcat /proc/config.gz > .config && make olddefconfig
-if [ $? -ne 0 ]; then
-    echo "Failed to build old default config!"
-    exit 1
-fi
+should_compile=`echo "${res}" | grep "Already up-to-date"`
+if [[ -z "${should_compile}" ]]; then
+    cd ${linux_dir}
+    zcat /proc/config.gz > .config && make olddefconfig
+    if [ $? -ne 0 ]; then
+        echo "Failed to build old default config!"
+        exit 1
+    fi
 
-make && make modules_install INSTALL_MOD_PATH=${install_mod_path} && cd -
-if [ $? -ne 0 ]; then
-    echo "Failed to build the kernel!"
-    exit 1
+    make && make modules_install INSTALL_MOD_PATH=${install_mod_path} && cd -
+    if [ $? -ne 0 ]; then
+        echo "Failed to build the kernel!"
+        exit 1
+    fi
+else
+    echo "Linux: No new commits, skipping building!"
 fi
 
 # Build Nouveau DRM kernel module.
 if [ -d ${nouveau_dir} ]; then
     cd ${nouveau_dir}
-    git fetch origin && git rebase origin/master
+    git fetch origin
+    res=`git rebase origin/master`
     if [ $? -ne 0 ]; then
         echo "Failed to update 'nouveau' repository!"
         exit 1
@@ -49,12 +57,17 @@ else
     fi
 fi
 
-linux_version=$(cat ${linux_dir}/include/config/kernel.release)
-linuxdir=$(echo "$PWD/lib/modules/${linux_version}/source")
+should_compile=`echo "${res}" | grep "Current branch master is up to date."`
+if [[ -z "${should_compile}" ]]; then
+    linux_version=$(cat ${linux_dir}/include/config/kernel.release)
+    linuxdir=$(echo "$PWD/lib/modules/${linux_version}/source")
 
-cd ${nouveau_dir}
-make && cd drm && make LINUXDIR=${linuxdir} && cd -
-if [ $? -ne 0 ]; then
-    echo "Failed to build 'nouveau'!"
-    exit 1
+    cd ${nouveau_dir}
+    make && cd drm && make LINUXDIR=${linuxdir} && cd -
+    if [ $? -ne 0 ]; then
+        echo "Failed to build 'nouveau'!"
+        exit 1
+else
+    echo "Nouveau: No new commits, skipping building!"
+    fi
 fi
